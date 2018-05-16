6e4777a3f36a35f527c3b688d4e6579be41411aa
ano21lxx <ano21lxx@bitbucket.org>
2018-05-17 00:03:14 +0800

    Base Template

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from home.views import HomeView
 
 app_name = 'home'
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
-    
+
     def home(self, request):
         return render(request, self.template_name)
+
